bc373bfb46026c539688f42c6741003a6686579f
fendy1996 <fendy1996fast@hotmaill.com>
2017-10-05 11:27:43 +0800

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,19 @@
+var hellomessage = "Hello Wourld!";
+console.log(hellomessage);
+
+var fruit ="apple"
+var applenum ="3";
+console.log(fruit);
+console.log(applenum);
+
+var 蘋果派 = "3";
+console.log(蘋果派);
+
+var studentnum=7, teachernum =1;
+var allpeoplenum= studentnum+teachernum;
+console.log(allpeoplenum)
+
+var question= "What's your name?", answer =" Fendy.";
+console.log(question+answer);
+var agequestion= "How old are you?", answer= " 21.";
+console.log(agequestion+answer);
