eeee0fb12183c29240e2dbe796007d34af384d99
fendy1996 <fendy1996fast@hotmaill.com>
2017-10-05 11:59:39 +0800

    for

--- client/main.js
+++ client/main.js
@@ -9,11 +9,70 @@ console.log(applenum);
 var 蘋果派 = "3";
 console.log(蘋果派);
 
-var studentnum=7, teachernum =1;
+var studentnum=7, teachernum =2;
 var allpeoplenum= studentnum+teachernum;
 console.log(allpeoplenum)
+
+if(allpeoplenum!==9)
+{
+  console.log("what?")
+}
+else {
+  console.log("good")
+}
+
 
 var question= "What's your name?", answer =" Fendy.";
 console.log(question+answer);
 var agequestion= "How old are you?", answer= " 21.";
 console.log(agequestion+answer);
+
+var person="Tod";
+if(person==="Fendy")
+{
+  console.log("HELLOOO!")
+}
+else
+{
+  console.log("Whyyy are you here?");
+}
+
+var IQ = 180
+if(IQ>= 180)
+{
+  console.log("Woohoo!")
+}
+else{
+  console.log("poor you!")
+}
+
+var language = "english"
+if (language ==="english")
+{
+  console.log("HELLO!")
+}
+else if(language === "French")
+{ console.log("bon jour!")}
+
+else
+{console.log("@__@")}
+
+for(age= 0; age  < 100 ; age++)
+{
+  if(age ===0 )
+  {
+    console.log(age+":hello world!")
+  }
+  else if(age ===25)
+  {
+    console.log (age+":you are so energetic!")
+  }
+  else if (age ===75)
+  {
+    console.log (age+":you have changed a lot...")
+  }
+  else if (age ===99)
+  {
+    console.log (age+":heaven or hail?")
+  }
+}
