c2476b18f005e22714196993463979dedd9cac41
fendy1996 <fendy1996fast@hotmaill.com>
2017-10-05 12:36:24 +0800

    function detail

--- client/main.js
+++ client/main.js
@@ -1,78 +1,22 @@
-var hellomessage = "Hello Wourld!";
-console.log(hellomessage);
 
-var fruit ="apple"
-var applenum ="3";
-console.log(fruit);
-console.log(applenum);
+var name1 ="Ray", age1 =18;
+var name2 ="Eve", age2 =26;
 
-var 蘋果派 = "3";
-console.log(蘋果派);
-
-var studentnum=7, teachernum =2;
-var allpeoplenum= studentnum+teachernum;
-console.log(allpeoplenum)
-
-if(allpeoplenum!==9)
+var agecomment = function(name, age)
 {
-  console.log("what?")
-}
-else {
-  console.log("good")
-}
-
-
-var question= "What's your name?", answer =" Fendy.";
-console.log(question+answer);
-var agequestion= "How old are you?", answer= " 21.";
-console.log(agequestion+answer);
-
-var person="Tod";
-if(person==="Fendy")
-{
-  console.log("HELLOOO!")
-}
-else
-{
-  console.log("Whyyy are you here?");
-}
-
-var IQ = 180
-if(IQ>= 180)
-{
-  console.log("Woohoo!")
-}
-else{
-  console.log("poor you!")
-}
-
-var language = "english"
-if (language ==="english")
-{
-  console.log("HELLO!")
-}
-else if(language === "French")
-{ console.log("bon jour!")}
-
-else
-{console.log("@__@")}
-
-for(age= 0; age  < 100 ; age++)
-{
-  if(age ===0 )
+  var msg;
+  if (age<= 20)
   {
-    console.log(age+":hello world!")
+    msg = name+",you are so young!";
   }
-  else if(age ===25)
-  {
-    console.log (age+":you are so energetic!")
-  }
-  else if (age ===75)
-  {
-    console.log (age+":you have changed a lot...")
-  }
-  else if (age ===99)
-  {
-    console.log (age+":heaven or hail?")
-  }
+  else
+    {
+    msg= name+", you are an adult....";
+    }
+  return msg;
 }
+
+var comment= agecomment(name1,age1);
+console.log(comment);
+var comment = agecomment(name2,age2);
+console.log(comment);
