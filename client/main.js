dc026f259af824bb47ff56e236aa92cf306bb0ef
fendy1996 <fendy1996fast@hotmaill.com>
2017-10-19 14:00:13 +0800

    first success!

--- client/main.js
+++ client/main.js
@@ -13,7 +13,41 @@ Template.body.events({
 }
 }});
 
+Template.body.events({
+  "click #submitmsg": function(event)
+  {event.preventDefault();
+    /*var formula ="67/7-150";
+    var results =eval(formula);
+    console.log (results);
 
+    var texts= "This is a string.";
+    var position = texts.indexOf("how much is?")
+    console.log(position);
+
+    var length = texts.length;
+    console.log(length);
+
+    var substring = texts.substring(0,17)
+    console.log(substring);*/
+
+    var conversationbox = document.getElementById("conversationbox");
+
+    var allMsgs = conversationbox.value;
+
+    var myMsg = document.getElementById("myMsg").value;
+    myMsg = "\nYou: "+ myMsg;
+
+    allMsgs = allMsgs+ myMsg;
+
+    conversationbox.value = allMsgs;
+
+    var newElizaMsg = "\nEliza: what's up?"
+
+    allMsgs = allMsgs+ newElizaMsg ;
+
+    console.log(allMsgs);
+    }
+  });
 
 
 /*var name1 ="Ray", age1 =18;
@@ -38,38 +72,7 @@ console.log(comment);
 var comment = agecomment(name2,age2);
 console.log(comment);
 
-Template.body.events({
-  "click #submitmsg": function(event)
-  {event.preventDefault();
-    var formula ="67/7-150";
-    var results =eval(formula);
-    console.log (results);
 
-    var texts= "This is a string.";
-    var position = texts.indexOf("how much is?")
-    console.log(position);
-
-    var length = texts.length;
-    console.log(length);
-
-    var substring = texts.substring(0,17)
-    console.log(substring);
-    //var conversationbox = document.getElementById("conversationbox");
-
-    //var allMsgs = conversationbox.value;
-
-    //var newMsg = document.getElementById("newMsg");
-    //newMsg = "\nYou: "+ newMsg;
-
-    //allMsgs = allMsgs+ newMsg;
-
-    //conversationbox.value = allMsgs;
-
-    //var newElizaMsg = "\nEliza: what's up?"
-
-    //allMsgs = allMsgs+ newElizaMsg ;
-
-    //console.log(allMsgs);
   //alert("Button Clicked!");
 
     //console.log(newMsg);
